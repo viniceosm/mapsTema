ef07b584263f5207985b4634ee7cf7d2b53b8982
viniceosm <miiller.vini@gmail.com>
2018-11-09 16:29:57 -0200

    Array de obj para opções de tipos de lugares, vem selecionado night_club

--- place_nearby.js
+++ place_nearby.js
@@ -8,6 +8,8 @@ var typesTextSearch = new Set();
 var radiusNearby = 1000;
 var markersPlaceNearby = [];
 
+var opTypes = getOpTypes();
+
 window.onload = function () {
 	document.getElementById('addType').onclick = function () {
 		adicionaTipo();
@@ -18,6 +20,15 @@ window.onload = function () {
 			adicionaTipo();
 		}
 	};
+
+	for (op of opTypes) {
+		document.getElementById('types').innerHTML += `<option value="${ op.value }">${ op.text }</option>`;
+	}
+
+	if (opTypes !== undefined && opTypes.findIndex((o) => o.value == 'night_club') > -1) {
+		document.getElementById('types').value = 'night_club';
+	}
+
 
 	document.getElementById('myRange').oninput = function () {
 		var m = document.getElementById('myRange').value;
@@ -58,7 +69,6 @@ function initMap() {
 		center: currentLocal,
 		zoom: 15
 	});
-	console.log('maps ae');
 
 	infowindow = new google.maps.InfoWindow();
 	service = new google.maps.places.PlacesService(map);
@@ -68,8 +78,6 @@ function initMap() {
 
 async function pesquisaLugaresPertos() {
 	if (typesTextSearch.size > 0) {
-		console.log('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa');
-
 		radiusNearby = document.getElementById('myRange').value;
 
 		for (type of typesSearch) {
@@ -85,8 +93,6 @@ async function pesquisaLugaresPertos() {
 				}
 			}
 		}
-	} else {
-		console.log('vvvvvvvvvvvvvvvvvvvvvvvvvvv');
 	}
 }
 
@@ -105,10 +111,13 @@ function createMarkerPlaceNearby(place) {
 	});
 
 	markersPlaceNearby.push(markerPlaceNearby);
-	console.log('criou o markeer');
 
 	google.maps.event.addListener(markerPlaceNearby, 'mouseover', function () {
 		infowindow.setContent(place.name);
 		infowindow.open(map, this);
 	});
+}
+
+function getOpTypes () {
+	return [{ "value": "accounting", "text": "contabilidade" }, { "value": "airport", "text": "aeroporto" }, { "value": "amusement_park", "text": "parque de diversões" }, { "value": "aquarium", "text": "aquário" }, { "value": "art_gallery", "text": "galeria de arte" }, { "value": "atm", "text": "atm" }, { "value": "bakery", "text": "padaria" }, { "value": "bank", "text": "banco" }, { "value": "bar", "text": "bar" }, { "value": "beauty_salon", "text": "salão de beleza" }, { "value": "bicycle_store", "text": "loja de bicicletas" }, { "value": "book_store", "text": "loja de livro" }, { "value": "bowling_alley", "text": "boliche" }, { "value": "bus_station", "text": "estação de onibus" }, { "value": "cafe", "text": "café" }, { "value": "campground", "text": "área de camping" }, { "value": "car_dealer", "text": "vendedor de carros" }, { "value": "car_rental", "text": "aluguer de carros" }, { "value": "car_repair", "text": "reparo de carro" }, { "value": "car_wash", "text": "lavação de carro" }, { "value": "casino", "text": "casino" }, { "value": "cemetery", "text": "cemiterio" }, { "value": "church", "text": "igreja" }, { "value": "city_hall", "text": "prefeitura" }, { "value": "clothing_store", "text": "loja de roupas" }, { "value": "convenience_store", "text": "loja de conveniencia" }, { "value": "courthouse", "text": "tribunal" }, { "value": "dentist", "text": "dentista" }, { "value": "department_store", "text": "loja de departamento" }, { "value": "doctor", "text": "doutor" }, { "value": "electrician", "text": "eletricista" }, { "value": "electronics_store", "text": "lojas eletronicas" }, { "value": "embassy", "text": "embaixada" }, { "value": "fire_station", "text": "corpo de bombeiros" }, { "value": "florist", "text": "florista" }, { "value": "funeral_home", "text": "funeraria" }, { "value": "furniture_store", "text": "loja de moveis" }, { "value": "gas_station", "text": "posto de gasolina" }, { "value": "gym", "text": "academia" }, { "value": "hair_care", "text": "cuidado capilar" }, { "value": "hardware_store", "text": "loja de hardware" }, { "value": "hindu_temple", "text": "templo hindu" }, { "value": "home_goods_store", "text": "loja de artigos para o lar" }, { "value": "hospital", "text": "hospital" }, { "value": "insurance_agency", "text": "agência de seguros" }, { "value": "jewelry_store", "text": "joalheria" }, { "value": "laundry", "text": "lavanderia" }, { "value": "lawyer", "text": "advogado" }, { "value": "library", "text": "biblioteca" }, { "value": "liquor_store", "text": "loja de bebidas" }, { "value": "local_government_office", "text": "secretaria do governo local" }, { "value": "locksmith", "text": "chaveiro" }, { "value": "lodging", "text": "alojamento" }, { "value": "meal_delivery", "text": "entrega de refeições" }, { "value": "meal_takeaway", "text": "drive thru" }, { "value": "movie_rental", "text": "aluguel de filmes" }, { "value": "movie_theater", "text": "cinema" }, { "value": "moving_company", "text": "companhia movente" }, { "value": "museum", "text": "museu" }, { "value": "night_club", "text": "night club" }, { "value": "painter", "text": "pintor" }, { "value": "park", "text": "park" }, { "value": "parking", "text": "parking" }, { "value": "pet_store", "text": "loja de animais" }, { "value": "pharmacy", "text": "farmacia" }, { "value": "physiotherapist", "text": "Fisioterapeuta" }, { "value": "plumber", "text": "encanador" }, { "value": "police", "text": "policia" }, { "value": "post_office", "text": "agencia de correios" }, { "value": "real_estate_agency", "text": "agencia imobiliaria" }, { "value": "restaurant", "text": "restaurante" }, { "value": "roofing_contractor", "text": "empreiteiro de coberturas" }, { "value": "rv_park", "text": "rv park" }, { "value": "school", "text": "escola" }, { "value": "shoe_store", "text": "loja de tenis" }, { "value": "shopping_mall", "text": "shopping" }, { "value": "spa", "text": "spa" }, { "value": "stadium", "text": "estadio" }, { "value": "storage", "text": "armazenam" }, { "value": "store", "text": "loja" }, { "value": "subway_station", "text": "estação de metrô" }, { "value": "supermarket", "text": "super mercado" }, { "value": "synagogue", "text": "sinagoga" }, { "value": "taxi_stand", "text": "ponto de taxi" }, { "value": "train_station", "text": "estação de trem" }, { "value": "transit_station", "text": "estação de transito" }, { "value": "travel_agency", "text": "agencia de viagem" }, { "value": "veterinary_care", "text": "veterinario" }, { "value": "zoo", "text": "zoo" }];
 }
